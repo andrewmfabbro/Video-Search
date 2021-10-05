84d99bf7853d14cec0295ebdf3cf6ad66626e828
Andrew <andrewmfabbro@gmail.com>
2021-10-05 14:09:21 -0400

    Updates

---
+++ precache-manifest.d981d51b72e84096c291dfe70eb79801.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c025c1614aa078ac85e7a8577cb1542",
+    "url": "/Video-Search/index.html"
+  },
+  {
+    "revision": "f586174305f861427184",
+    "url": "/Video-Search/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "2d0751566b0f234c2e94",
+    "url": "/Video-Search/static/js/2.f1b9daee.chunk.js"
+  },
+  {
+    "revision": "f586174305f861427184",
+    "url": "/Video-Search/static/js/main.ec97762a.chunk.js"
+  },
+  {
+    "revision": "f1e340510b658774a42f",
+    "url": "/Video-Search/static/js/runtime-main.0b71ac4b.js"
+  }
+]);
